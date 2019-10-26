9217b9bed77a4726a68f3bfaa4e50be2f71f1d3e
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2019-10-26 21:00:38 +0200

    an example with C-SVC

--- svm/generate_images.py
+++ svm/generate_images.py
@@ -10,26 +10,26 @@ import svm_tools
 import plot_tools
 
 
-def train_classifier(n_samples,
-                     sigma):
-    X, y = sklearn.datasets.make_moons(n_samples=n_samples, noise=0.25)
+def train_classifier(build_clf,
+                     n_samples):
+    X, y = sklearn.datasets.make_moons(n_samples=n_samples, noise=0.1)
 
     # Let us learn a linear C-SVC
-    gamma = 1.0 / (2.0 * sigma**2)
-    classifier = sklearn.svm.NuSVC(nu=0.3,
-                                   kernel='rbf', gamma=gamma,
-                                   tol=1e-5,
-                                   decision_function_shape='ovo')
+    classifier = build_clf()
     classifier.fit(X, y)
 
     return classifier, (X, y)
 
 
 def example001():
-    clf, (X, y) = train_classifier(400, sigma=0.4)
+    build_clf = lambda: sklearn.svm.SVC(C=1,
+                                        kernel='rbf', gamma=1/(2.0 * 0.4**2),
+                                        tol=1e-5,
+                                        decision_function_shape='ovo')
+    clf, (X, y) = train_classifier(build_clf, 100)
     sep, desc, info = svm_tools.svc_ovo_separators(clf,
                                                    svm_tools.gaussian_kernel(clf.gamma), X)
-    fig = plt.figure(figsize=(12,10))
+    fig = plt.figure(figsize=(12, 10))
 
     xlim = [-1.5, 2.5]
     ylim = [-1.5, 1.5]
@@ -38,13 +38,14 @@ def example001():
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     ax.set_aspect('equal')
-    plot_tools.plot_svc_samples(ax, desc, info, X, y,
+    plot_tools.plot_svc_samples(ax, desc, info, X[:100], y[:100],
                                 '0 vs 1', 'white', 'gray',
                                 'black', 'red')
     plot_tools.plot_svc_separation(ax, sep, info, '0 vs 1', xlim, ylim)
     plot_tools.plot_svc_partition(ax, clf,
                                   xlim, ylim,
-                                  [(.98, .98, 1.), (.98, 1., .98)])
+                                  [(.8, .8, 1.), (.8, 1., .8)])
+    plt.savefig('csvc001.png', bbox_inches='tight')
     plt.show()
 
 
